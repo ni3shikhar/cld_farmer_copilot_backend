e6f8e876ee3cfe1d5cdabe9e340602985d77e37a
Nitin S <nms@gmail.com>
2025-06-03 13:48:02 +0000

    ee

--- __init.__.py
+++
@@ -1,8 +0,0 @@
-# analyze/__init__.py
-
-import azure.functions as func
-from azure.functions import AsgiMiddleware
-from .main import app  # ← Import the FastAPI app from main.py
-
-# Azure Functions entry point for HTTP trigger
-main = AsgiMiddleware(app)
